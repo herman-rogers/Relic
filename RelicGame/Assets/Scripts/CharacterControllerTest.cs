89f651c2ca66e118d0640d1f068fc370336aaf9b
Admin <herman.rogers@gmail.com>
2014-04-03 12:03:44 -0600

    Cleaning up CharacterController scripts, removing old Monster Animation script, cleaning up meta files, re-adding new metafiles to use charactercontroller

--- RelicGame/Assets/Scripts/CharacterControllerTest.cs
+++ RelicGame/Assets/Scripts/CharacterControllerTest.cs
@@ -1,12 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CharacterControllerTest : MonoBehaviour {
-	public dyCharacterController characterController;
+	public CharacterController characterController;
 	
 	void Start( ){
 	characterController.MoveMonsterOnXAxis( 10.0f );
-	characterController.BlendNewMovement( -10.0f, dyCharacterController.AnimationList.Running );
+	characterController.BlendNewMovement( -10.0f, CharacterController.AnimationList.Running );
 	//characterController.BlendNewMovement( 10.0f, dyCharacterController.AnimationList.Running );
 	//	characterController.BlendNewAnimation( dyCharacterController.AnimationList.Running );
 	}
---
+++ RelicGame/Assets/Scripts/CharacterPressGesture.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using TouchScript;
+using TouchScript.Gestures;
+
+public class CharacterPressGesture : MonoBehaviour {
+	private CharacterController moveMonster;
+	private PressGesture pressGesture;
+
+	void Start( ){
+		pressGesture = this.GetComponent< PressGesture >( );
+		moveMonster = this.GetComponent< CharacterController >( );
+		if( pressGesture != null ) pressGesture.StateChanged += StateChangeHandler;
+	}
+
+	void StateChangeHandler( object sender, TouchScript.Events.GestureStateChangeEventArgs e ){
+		switch( e.State ){
+		case Gesture.GestureState.Recognized:
+			moveMonster.MoveMonsterOnXAxis( pressGesture.ScreenPosition.x, 
+			                                CharacterController.AnimationList.Walking, true );
+			break;
+		}
+	}
+}
