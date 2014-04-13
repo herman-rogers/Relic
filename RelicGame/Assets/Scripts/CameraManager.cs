812391656d43fad52723776a34b93cbabdf94b16
James Timms PC <James-Timms@hotmail.com>
2014-04-13 16:06:27 +0100

    Adding stuff.

--- RelicGame/Assets/Scripts/CameraManager.cs
+++ RelicGame/Assets/Scripts/CameraManager.cs
@@ -6,39 +6,22 @@ using TouchScript.Events;
 public class CameraManager : MonoBehaviour {
 
 	public GameObject monster;
-	public Vector2    firstTouchPosition;
-	public Vector2    secondTouchPosition;
-	public Vector2    currentTouchMovement;
-	public bool       hasTouchEnded{ get; private set; }
-	bool              isMoving = false;
+	[ Range( 0.1f, 5.0f ) ]
+	public float cameraMoveSpeed = 0.5f;
+	bool isMoving = false;
 	public static Vector3 cameraPos;
 
-	void Start( ) {
-//		TouchManager.Instance.TouchesBegan += OnTouchBegin;
-//		TouchManager.Instance.TouchesMoved += OnTouchContinue;
-//		TouchManager.Instance.TouchesEnded += OnTouchEnd;
+	public Transform topLeftAnchor;
+	public Transform botRightAnchor;
+	bool shouldCheck;
+	float cameraHalfWidth;
+
+	private void Awake( ) {
+		shouldCheck = (topLeftAnchor != null && botRightAnchor != null);
+		cameraHalfWidth = ( camera.orthographicSize * camera.aspect );
 	}
 
-	public void OnTouchBegin( object sender, TouchEventArgs e ) {
-		foreach( TouchPoint point in e.TouchPoints ) {
-			firstTouchPosition = new Vector2( point.Position.x, point.Position.y );
-		}
-	}
-
-	public void OnTouchContinue( object sender, TouchEventArgs e ) {
-		foreach( var point in e.TouchPoints ) {
-			secondTouchPosition  = new Vector2( point.Position.x, point.Position.y );
-			currentTouchMovement = new Vector2( ( secondTouchPosition.x - firstTouchPosition.x ),
-			                                    ( secondTouchPosition.y - firstTouchPosition.y ));
-			currentTouchMovement.Normalize( );
-		}
-    }
-
-    public void OnTouchEnd( object sender, TouchEventArgs e ) {
-		hasTouchEnded = true;
-    }
-
-    public void Update( ) {
+	public void Update( ) {
 		if( !isMoving && ( this.camera.WorldToScreenPoint( monster.transform.position ) ).x < 250.0f || 
 		   !isMoving && ( ( (float)Screen.width ) - this.camera.WorldToScreenPoint( monster.transform.position ).x ) < 250.0f ) {
 			isMoving = true;
@@ -46,18 +29,30 @@ public class CameraManager : MonoBehaviour {
 		}
 		else if( isMoving ) {
 			LerpCamera( );
-			if( ( this.camera.WorldToScreenPoint(monster.transform.position ) ).x > 300.0f && 
-			   ( ( (float)Screen.width ) - this.camera.WorldToScreenPoint( monster.transform.position ).x ) > 300.0f ) {
-				isMoving = false;
-			}
+			isMoving = !( ( this.camera.WorldToScreenPoint(monster.transform.position ) ).x > 300.0f && 
+			             ( ( (float)Screen.width ) - this.camera.WorldToScreenPoint( monster.transform.position ).x ) > 300.0f );
 		}
 	}
 
 	void LerpCamera( ) {
 		Vector3 monsterX = monster.transform.position;
+		if( shouldCheck ) {
+			bool canMove = ( monsterX.x > this.transform.position.x ) ? CanMoveRight( ) : CanMoveLeft( );
+			if( !canMove ) {
+				return;
+			}
+		}
 		monsterX.y = this.transform.position.y;
 		monsterX.z = this.transform.position.z;
-		this.transform.position = Vector3.Lerp( this.transform.position, monsterX, 0.5f * Time.deltaTime );
+		this.transform.position = Vector3.Lerp( this.transform.position, monsterX, cameraMoveSpeed * Time.deltaTime );
 		cameraPos = this.transform.position;
+	}
+
+	bool CanMoveLeft( ) {
+		return ( this.transform.position.x - cameraHalfWidth > topLeftAnchor.position.x );
+	}
+	
+	bool CanMoveRight( ) {
+		return ( this.transform.position.x + cameraHalfWidth < botRightAnchor.position.x );
 	}
 }
