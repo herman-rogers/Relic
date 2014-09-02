6dbb866c1a380d7ba80f61e78eff8672232f0772
James Timms PC <James-Timms@hotmail.com>
2014-09-02 02:33:07 +0100

    Added a simple inspector script for Polygon.cs

---
+++ RelicGame/Assets/Editor/Navigation/PolygonInspector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor ( typeof ( Polygon ) )]
+public class PolygonInspector : Editor {
+    
+    public override void OnInspectorGUI( ) {
+        Polygon polygon = ( Polygon ) target;
+
+        Vector2[ ] polyCorners = polygon.polygonCorners;
+        int numberOfCorners = EditorGUILayout.IntField( "Number Of Corners", polyCorners.Length );
+
+        for( int num = 0; num < polyCorners.Length; num++ ) {
+            polyCorners[ num ] = EditorGUILayout.Vector2Field( "Corner " + num.ToString( ), polyCorners[ num ] );
+        }
+
+        CheckForResize( polygon, polyCorners, numberOfCorners );
+    }
+
+    private static void CheckForResize( Polygon polygon, Vector2[ ] polyCorners, int numberOfCorners ) {
+        if( numberOfCorners > polyCorners.Length ) {
+            Vector2[ ] newCorners = new Vector2[ numberOfCorners ];
+            polyCorners.CopyTo( newCorners, 0 );
+            polygon.polygonCorners = newCorners;
+        } else if( numberOfCorners < polyCorners.Length ) {
+            Vector2[ ] newCorners = new Vector2[ numberOfCorners ];
+            for( int i = 0; i < numberOfCorners; i++ ) {
+                newCorners[ i ] = polyCorners[ i ];
+            }
+            polygon.polygonCorners = newCorners;
+        }
+    }
+
+}
