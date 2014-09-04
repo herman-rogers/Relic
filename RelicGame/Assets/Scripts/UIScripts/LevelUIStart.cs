dd65dec8359b60e0b8e18378f6d3a67c6e14de3b
Boomer <herman.rogers@gmail.com>
2014-09-03 18:21:10 -0800

    Stashing for pulling

--- RelicGame/Assets/Scripts/UIScripts/LevelUIStart.cs
+++ RelicGame/Assets/Scripts/UIScripts/LevelUIStart.cs
@@ -3,25 +3,28 @@ using System.Collections;
 
 public class LevelUIStart : UnityObserver
 {
-	public UITexture fadeObjects;
+	public UITexture fadeBackground;
 	public UILabel test;
 	private const float startDelay = 5.0f;
 	private const float fadeTime = 0.009f;
 
-	void Awake( )
-	{
+	void Awake( ) {
 		CharacterController.togglePlayerInput = PlayerInput.STOP_PLAYER_INPUT;
-		fadeObjects.alpha = 1.0f;
+		SetBackgroundToScreenSize( );
+		fadeBackground.alpha = 1.0f;
 		test.alpha = 0.0f;
 		StartCoroutine( FadeBackground( ) );
 	}
 
-	private IEnumerator FadeBackground( )
-	{
+	private void SetBackgroundToScreenSize( ) {
+		fadeBackground.GetComponent< UITexture >( ).width = Screen.width;
+		fadeBackground.GetComponent< UITexture > ().height = Screen.height;
+	}
+
+	private IEnumerator FadeBackground( ) {
 		yield return new WaitForSeconds( startDelay );
-		while( fadeObjects.alpha > 0.0f )
-		{
-			fadeObjects.alpha -= fadeTime;
+		while( fadeBackground.alpha > 0.0f ) {
+			fadeBackground.alpha -= fadeTime;
 			yield return new WaitForSeconds( fadeTime );
 		}
 		CharacterController.togglePlayerInput = PlayerInput.START_PLAYER_INPUT;
